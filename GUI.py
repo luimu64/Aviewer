f489866cc900399341342ed42ce7267328b6a700
luimu <luimu@localhost.localdomain>
2020-05-24 03:07:28 +0300

    fixes and threading

--- GUI.py
+++ GUI.py
@@ -1,15 +1,15 @@
 from subprocess import run
 import PySimpleGUI as sg
-
+import _thread
 from animescript import anime
 
 x = anime()
 
 
-sg.theme('DarkRed')
+sg.theme('Dark')
 
-layout = [  [sg.InputText('Search something...'), sg.Button('search'), sg.InputText('Ordinal...', size=(15, 0)), sg.Button('refresh'), sg.InputText('Episode number...', size=(16, 0)), sg.Button('play and get link')],
-            [sg.Output(size=(98,20))]]
+layout = [  [sg.InputText('Search something...'), sg.Button('search'), sg.InputText('Ordinal...', size=(15, 0)), sg.Button('refresh'), sg.InputText('Episode number...', size=(16, 0)), sg.Button('play and get links')],
+            [sg.Output(size=(115,20))]]
 
 mainw = sg.Window('Aviewer', layout)
 
@@ -17,7 +17,7 @@ while True:
     event, values = mainw.read()
     if event in (None, 'Cancel'):
         break
-    if event in (None, 'play and get link'):
+    if event in (None, 'play and get links'):
         if hasattr(x, "results") == False:
                 print("You have to search before you can get episodes.\n")
         else:
@@ -26,10 +26,11 @@ while True:
             if x.cleanlinks:
                 print(x.cleanlinks)
                 mainw.refresh()
-                run([
+                _thread.start_new_thread(run, ([
                     "mpv",
                     x.cleanlinks[0]
-                ])
+                ],)
+                )
             else:
                 print("Sorry, I couldn't find the source links :(")
     if event in (None, 'refresh'):
